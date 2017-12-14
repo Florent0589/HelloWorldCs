c99aab5462382092eb757993a92061b73a6a27b0
Ayanda Mavimbela <34153569+Florent0589@users.noreply.github.com>
2017-12-14 13:53:08 +0200

    Create README.md

--- Program.cs
+++ Program.cs
@@ -10,11 +10,14 @@ namespace HelloWorldCs
     {
         static void Main(string[] args)
         {
+            // setting used variables
             string name = "Florent Shomora";
-
+            string appName = "Welcome To C# Programming";
+            string version = "1.1.2";
+            string appYear = "2017/12/14";
 
             // start here 
-            Console.WriteLine("Hello "+ name +", Welcome to C# Programming.");
+            Console.WriteLine("{0},  {1} , Version : {2}. Released Date : {3}", name, appName, version, appYear);
             Console.ReadLine();
         }
     }
