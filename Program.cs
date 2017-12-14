89f46109e4c39c05751c25e4306fc4c55a55071d
Florent0589 <34153569+Florent0589@users.noreply.github.com>
2017-12-14 14:31:19 +0200

    using variables in c#

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloWorldCs
 {
@@ -15,6 +11,7 @@ namespace HelloWorldCs
             string appName = "Welcome To C# Programming";
             string version = "1.1.2";
             string appYear = "2017/12/14";
+
 
             // start here 
             Console.WriteLine("{0},  {1} , Version : {2}. Released Date : {3}", name, appName, version, appYear);
