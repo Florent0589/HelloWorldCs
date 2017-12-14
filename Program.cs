fbd440c6b933b31c559842becc1649ae39cfdb18
Florent0589 <34153569+Florent0589@users.noreply.github.com>
2017-12-14 13:46:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Florent Shomora";
+
+
+            // start here 
+            Console.WriteLine("Hello "+ name +", Welcome to C# Programming.");
+            Console.ReadLine();
+        }
+    }
+}
